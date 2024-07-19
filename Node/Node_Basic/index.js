76f29c372f1d6448ae6b2c0a3232f500eada35b0
Kesar <kesar@Kunjals-Mac.local>
2024-07-19 11:07:21 +0530

    assignment of 18th july 24 added

---
+++ Node/Node_Basic/index.js
@@ -0,0 +1,5 @@
+//in class task - 1 =>
+const {var1, sum} = require('./module')
+
+console.log(var1)
+console.log(sum(45,54))
---
+++ Node/Node_Basic/module.js
@@ -0,0 +1,7 @@
+//in class task - 1 =>
+let var1 = 77
+ function sum(a,b){
+    return a+b
+ }
+
+ module.exports = {var1, sum}
