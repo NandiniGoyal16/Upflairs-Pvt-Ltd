33a5212d3630cbd64f4819369c0ca31904b0a658
Kesar <kesar@Kunjals-Mac.local>
2024-07-26 03:06:57 +0530

    added login and signUp functionality

--- E-commerce/backend/index.js
+++ E-commerce/backend/index.js
@@ -4,6 +4,8 @@ import { randomUUID } from "crypto";
 import express from "express";
 import mongoose from "mongoose";
 import multer from "multer";
+import jwt from "jsonwebtoken";
+
 
 const app = express();
 app.use(express.json());
@@ -87,6 +89,81 @@ app.post("/products/delete", async (req, res) => {
     name: product.name,
   });
 });
+
+//Schema creating for user model
+
+const Users = mongoose.model('Users',{
+  name:{
+    type: String,
+  },
+    email:{
+      type: String,
+      unique: true,
+    },
+    password:{
+      type:String,
+    },
+    cartData:{
+      type:Object,
+    },
+    date:{
+      type: Date,
+      default:Date.now,
+    }
+})
+
+//creating enpoint for registerng the user
+app.post('/signup', async (req,res)=>{
+  let check = await Users.findOne({email:req.body.email});
+  if(check){
+    return res.status(400).json({success:false,errors:"Existing user found with same email address"})
+  }
+  let cart = {};
+  for (let i = 0; i < 300; i++) {
+    cart[i]=0;
+  }
+  const user = new Users({
+    name:req.body.username,
+    email:req .body.email,
+    password:req.body.password,
+    cartData:cart,
+  })
+
+await user.save();
+
+const data = {
+  user:{
+    id:user.id
+  }
+}
+
+const token = jwt.sign(data, 'secret_ecom');
+res.json({success:true, token})
+
+})
+
+//creating endpoint for user login
+app.post('/login', async (req,res)=>{
+    let user = await Users.findOne({email:req.body.email});
+    if (user){
+      const passCompare = req.body.password === user.password;
+      if(passCompare){
+        const data = {
+          user:{
+            id:user.id
+          }
+        }
+        const token = jwt.sign(data, 'secret_ecom');
+        res.json({success:true, token})
+      }
+      else{
+        res.json({success:false, errors: "Wrong Password"});
+      }
+    }
+    else{
+      res.json({success:false, errors: "Wrong Email Address"})
+    }
+})
 
 app.listen(PORT, (error) => {
   if (!error) {
